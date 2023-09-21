f2ddc12a64cb7775477099dec9e6e2ceec1ba18e
Eric Kim <eric@blockthrough.com>
2023-09-21 16:30:59 -0400

    print sha

--- test/ci/build_templates.sh
+++ test/ci/build_templates.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "previous sha1: $CIRCLE_PREVIOUS_SHA1"
+echo "current sha1: $CIRCLE_SHA1"
+
 CHANGED_FILES=$(git diff --name-only $CIRCLE_PREVIOUS_SHA1..$CIRCLE_SHA1)
 echo "Changed files:"
 echo "$CHANGED_FILES"
