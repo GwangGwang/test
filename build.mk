4e579f63c1f4c37aff80f2def0fe119a81e3e951
Eric Gwangseung Kim <g28kim@gmail.com>
2023-10-02 10:22:17 -0400

    make

---
+++ build.mk
@@ -0,0 +1,18 @@
+
+define build-image
+	mkdir -p $1/out/
+	rm -rf $1/out/image.tar.gz
+	docker buildx build \
+		-f $1/Dockerfile \
+		--platform $(DOCKER_PLATFORM) \
+		--build-arg GOPROXY \
+		--build-arg CONFIG=$2 \
+		--build-arg COMMIT_SHA=$(COMMIT_SHA) \
+		--build-arg COMMIT_VERSION=$(COMMIT_VERSION) \
+		--compress \
+		-t $3 .
+	docker save $3 | gzip > $1/out/image.tar.gz
+endef
+
+build-events-image-dev:
+	$(call build-image,.,app.dev.json,events)
